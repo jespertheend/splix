184a950e970fd947fd4d1986e87a4832d3d2eb41
Jesper van den Ende <jespertheend@gmail.com>
2023-08-05 20:11:56 +0200

    Add importmap

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,5 +1,5 @@
 import { generateTypes } from "https://deno.land/x/deno_tsc_helper@v0.1.2/mod.js";
-import { setCwd } from "https://deno.land/x/chdir_anywhere@v0.0.3/mod.js";
+import { setCwd } from "chdir-anywhere";
 import { init } from "../src/mainInstance.js";
 setCwd();
 
@@ -10,8 +10,14 @@ generateTypes({
 		"scripts/",
 		"src/",
 	],
+	importMap: "importmap.json",
 	excludeUrls: [
-		"https://deno.land/x/deno_tsc_helper@v0.1.2/mod.js",
+		"https://deno.land/x/renda@v0.1.0/studio/src/styles/projectSelectorStyles.js",
+		"https://deno.land/x/renda@v0.1.0/studio/src/styles/studioStyles.js",
+		"https://deno.land/x/renda@v0.1.0/studio/src/styles/shadowStyles.js",
+		"https://deno.land/x/renda@v0.1.0/studio/deps/rollup-plugin-resolve-url-objects.js",
+		"https://deno.land/x/renda@v0.1.0/studio/deps/rollup.browser.js",
+		"rollup",
 	],
 	logLevel: "WARNING",
 });
