3ecfe1bdee60bcf29adf266f8919fcb1db0929a3
Jesper van den Ende <jespertheend@gmail.com>
2023-09-11 22:37:40 +0200

    only undo events between previousPosition and newPosition

--- gameServer/src/gameplay/PlayerEventHistory.js
+++ gameServer/src/gameplay/PlayerEventHistory.js
@@ -58,11 +58,54 @@ export class PlayerEventHistory {
 	 * @param {Vec2} newPosition
 	 */
 	undoRecentEvents(previousPosition, newPosition) {
+		// We create a rectangle which we can use to check which events lie within that rectangle.
+		// Any events outside of the rectangle should be ignored, otherwise we allow the player jump over
+		// certain tiles, such as player trails or the wall.
+		/** @type {import("../util/util.js").Rect} */
+		let allowedRect = {
+			min: new Vec2(),
+			max: new Vec2(),
+		};
+		if (previousPosition.x == newPosition.x && previousPosition.y == newPosition.y) {
+			// When the previous and new position are at the exact same location,
+			// no events should be undone because the player never moved back.
+			// We set the rectangle to something with size zero,
+			// That way all events will be filtered away
+			allowedRect.min.set(previousPosition);
+			allowedRect.max.set(previousPosition);
+		} else if (previousPosition.x == newPosition.x) {
+			if (previousPosition.y < newPosition.y) {
+				allowedRect.min.set(previousPosition.x, previousPosition.y);
+				allowedRect.max.set(newPosition.x + 1, newPosition.y);
+			} else {
+				allowedRect.min.set(newPosition.x, newPosition.y + 1);
+				allowedRect.max.set(previousPosition.x + 1, previousPosition.y + 1);
+			}
+		} else if (previousPosition.y == newPosition.y) {
+			if (previousPosition.x < newPosition.x) {
+				allowedRect.min.set(previousPosition.x, previousPosition.y);
+				allowedRect.max.set(newPosition.x, newPosition.y + 1);
+			} else {
+				allowedRect.min.set(newPosition.x + 1, newPosition.y);
+				allowedRect.max.set(previousPosition.x + 1, previousPosition.y + 1);
+			}
+		} else {
+			throw new Error("Assertion failed, previous and new position create a diagonal line.");
+		}
 		while (true) {
 			const event = this.#events.pop();
 			if (!event) break;
 			if (performance.now() - event.time > MAX_UNDO_EVENT_TIME) break;
-			this.#onUndoEventCbs.forEach((cb) => cb(event.event));
+
+			// Check if the event lies between previousPosition and newPosition, and skip otherwise
+			if (
+				event.position.x >= allowedRect.min.x &&
+				event.position.y >= allowedRect.min.y &&
+				event.position.x < allowedRect.max.x &&
+				event.position.y < allowedRect.max.y
+			) {
+				this.#onUndoEventCbs.forEach((cb) => cb(event.event));
+			}
 		}
 		this.#events = [];
 	}
