7fe1560d56d5e75f689be3891c7af237da5fcbbb
Jesper van den Ende <jespertheend@users.noreply.github.com>
2023-10-21 16:51:51 +0200

    fix(gs): Ignore ready state not open errors (#82)

--- gameServer/src/WebSocketConnection.js
+++ gameServer/src/WebSocketConnection.js
@@ -308,6 +308,9 @@ export class WebSocketConnection {
 		try {
 			this.#socket.send(data);
 		} catch (e) {
+			if (e instanceof DOMException && e.name == "InvalidStateError") {
+				return;
+			}
 			console.error("An error occurred while trying to send a message", data, e);
 			if (e instanceof Error) {
 				console.error(e.stack);
