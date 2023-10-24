13ad30247e49a8fc23d3c5494e084ff8e8db42f0
Jesper van den Ende <jespertheend@gmail.com>
2023-10-24 10:55:45 +0200

    feat(sm): Remove gameservers cache header

--- serverManager/src/WebSocketManager.js
+++ serverManager/src/WebSocketManager.js
@@ -35,7 +35,6 @@ export class WebSocketManager {
 					const data = mainInstance.servermanager.getServersJson();
 					const response = Response.json(data);
 					response.headers.set("Access-Control-Allow-Origin", "*");
-					response.headers.set("Cache-Control", "max-age=300");
 					return response;
 				}
 				return null;
