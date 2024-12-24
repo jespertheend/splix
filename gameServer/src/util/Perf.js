725d7693dfeb3b28c2c562e06abf1dcbbe6ac841
Dinoana <dinoana@proton.me>
2024-12-24 07:11:59 -0500

    perf(optimize): add performance measuring utility

---
+++ gameServer/src/util/Perf.js
@@ -0,0 +1,63 @@
+export class Perf {
+    /**
+     * @type {Object<string, { avgTime: number, totalTime: number, startTime: number, calls: number, lastElapsedTime: number }>}
+     */
+    static metrics = {};
+
+    /**
+     * @type {Object<string, number>}
+     */
+    static counter = {};
+
+    /**s
+     * @param {string} routineName
+     */
+    static start(routineName) {
+        if (!this.metrics[routineName]) {
+            this.metrics[routineName] = { avgTime: 0, totalTime: 0, lastElapsedTime: 0, calls: 0, startTime: 0 };
+        }
+        this.metrics[routineName].startTime = performance.now();
+    }
+
+    /**
+     * @param {string} routineName
+     */
+    static end(routineName) {
+        if (!this.metrics[routineName]) {
+            console.error(`<${routineName}>: start time is not set.`);
+            return;
+        }
+
+        const endTime = performance.now();
+        const entry = this.metrics[routineName];
+
+        if (entry.startTime === 0) {
+            console.error(`<${routineName}>: start time is not set.`);
+            return;
+        }
+
+        const elapsedTime = endTime - entry.startTime;
+        entry.totalTime += elapsedTime;
+        entry.calls += 1;
+        entry.avgTime = entry.totalTime / entry.calls;
+        entry.startTime = 0;
+        entry.lastElapsedTime = elapsedTime;
+    }
+
+    /**
+     * @param {string} counterName
+     */
+    static count(counterName) {
+        if (!this.counter[counterName]) {
+            this.counter[counterName] = 0;
+        }
+        this.counter[counterName] += 1;
+    }
+
+    static print() {
+        console.log(this.metrics);
+        if (Object.entries(this.counter).length) {
+            console.log(this.counter);
+        }
+    }
+}
