8018f5a6f31e122e723e3c71932ff313a4d24709
Dinoana <dinoana@proton.me>
2025-01-06 13:25:39 +0200

    refactor: more readable and lil more faster

--- gameServer/src/gameplay/arenaWorker/updateCapturedArea.js
+++ gameServer/src/gameplay/arenaWorker/updateCapturedArea.js
@@ -11,9 +11,10 @@ import { CircularQueue } from "../../util/CircularQueue.js";
  */
 let maskWidth = 0;
 let maskHeight = 0;
+let lineWidth = 0;
 
 /** @type {Uint8Array} */
-let byteArray;
+let grid;
 
 /** @type {CircularQueue} */
 let queue;
@@ -27,9 +28,14 @@ let queue;
 export function initializeMask(width, height) {
 	maskWidth = width;
 	maskHeight = height;
-	byteArray = new Uint8Array(maskWidth * maskHeight);
+	lineWidth = maskHeight;
+	grid = new Uint8Array(maskWidth * maskHeight);
 	queue = new CircularQueue(maskWidth * maskHeight); // maskWidth + maskHeight would be enough, but for safety
 }
+
+const FILLABLE_BLOCK = 0;
+const FILLED_BLOCK = 1;
+const PLAYER_BLOCK = 2;
 
 /**
  * Finds unfilled areas of the player and fills them.
@@ -46,78 +52,67 @@ export function updateCapturedArea(arenaTiles, playerId, bounds, unfillableLocat
 	bounds.min.subScalar(1);
 	bounds.max.addScalar(1);
 
+	// empty the queue.
+	queue.clear();
+
 	/**
-	 * Tests whether a node should be marked as 1 (unfillable by the player),
+	 * Tests if a node can be filled.
 	 * @param {number} x coord x
 	 * @param {number} y coord y
-	 * @param {number} index linear index -> x * maskHeight + y
+	 * @param {number} index linear index -> x * lineWidth + y
 	 * @returns {Boolean}
 	 */
 	function testFillNode(x, y, index) {
 		if (x < bounds.min.x || y < bounds.min.y) return false;
 		if (x >= bounds.max.x || y >= bounds.max.y) return false;
 
-		if (byteArray[index] == 1 || byteArray[index] === 2) return false;
+		if (grid[index] === FILLED_BLOCK || grid[index] === PLAYER_BLOCK) return false;
 		return true;
 	}
 
-	/**
-	 * Initialize the flood fill mask
-	 * -1: border
-	 *  0: initial state: area unfilled, but fillable by the player
-	 *  0: final state: area filled by the player
-	 *  1: area unfillable by the player
-	 *  2: already filled by the player
-	 */
 	for (let i = bounds.min.x; i < bounds.max.x; i++) {
-		const offset = i * maskHeight;
+		const offset = i * lineWidth;
 		for (let j = bounds.min.y; j < bounds.max.y; j++) {
 			if (arenaTiles[i][j] == playerId) {
-				byteArray[offset + j] = 2;
-			} else if (i == 0 || j == 0 || i == maskWidth - 1 || j == maskHeight - 1) {
-				byteArray[offset + j] = -1;
+				grid[offset + j] = PLAYER_BLOCK;
 			} else {
-				byteArray[offset + j] = 0;
+				grid[offset + j] = FILLABLE_BLOCK;
 			}
 		}
 	}
 
-	// We could seed the flood fill along anywhere across the edge of the bounds really,
-	// but we'll just go with the top left corner.
+	// pick the top left corner of the bounds as the floodfill seed, we are allowed to pick any point on the edge of the bounds.
 	const cornerSeed = bounds.min.clone();
 
 	// We do a quick assertion to make sure our seed is not outside the bounds or already owned by the player.
 	// There needs to be a border of one tile around the players area,
 	// otherwise the floodfill algorithm won't be able to fully wrap around the player area.
-	if (!testFillNode(cornerSeed.x, cornerSeed.y, cornerSeed.x * maskHeight + cornerSeed.y)) {
+	if (!testFillNode(cornerSeed.x, cornerSeed.y, cornerSeed.x * lineWidth + cornerSeed.y)) {
 		throw new Error("Assertion failed, expected the top left corner to get filled");
 	}
 
-	/**
-	 * clear the queue and enqueue the corner seed and mark it as 1 as it's unfillable by the player
-	 */
-	queue.clear();
+	// enqueue the corner seed and mark it as already filled
 	queue.enqueue(cornerSeed.x, cornerSeed.y);
-	byteArray[cornerSeed.x * maskHeight + cornerSeed.y] = 1;
+	grid[cornerSeed.x * lineWidth + cornerSeed.y] = FILLED_BLOCK;
 
 	// We also add seeds for all the player positions in the game,
 	// Since we don't want players to just fill a large area around another player.
 	for (const node of unfillableLocations) {
-		const offset = node[0] * maskHeight;
+		const offset = node[0] * lineWidth;
 		if (testFillNode(node[0], node[1] + 1, offset + node[1] + 1)) {
-			byteArray[offset + node[1] + 1] = 1;
+			grid[offset + node[1] + 1] = FILLED_BLOCK;
 			queue.enqueue(node[0], node[1] + 1);
 		}
 		if (testFillNode(node[0], node[1] - 1, offset + node[1] - 1)) {
-			byteArray[offset + node[1] - 1] = 1;
+			grid[offset + node[1] - 1] = FILLED_BLOCK;
 			queue.enqueue(node[0], node[1] - 1);
 		}
-		if (testFillNode(node[0] + 1, node[1], offset + maskHeight + node[1])) {
-			byteArray[offset + maskHeight + node[1]] = 1;
+		if (testFillNode(node[0] + 1, node[1], offset + lineWidth + node[1])) {
+			grid[offset + lineWidth + node[1]] = FILLED_BLOCK;
 			queue.enqueue(node[0] + 1, node[1]);
 		}
-		if (testFillNode(node[0] - 1, node[1], offset - maskHeight + node[1])) {
-			byteArray[offset - maskHeight + node[1]] = 1;
+		if (testFillNode(node[0] - 1, node[1], offset - lineWidth + node[1])) {
+			grid[offset - lineWidth + node[1]] = FILLED_BLOCK;
 			queue.enqueue(node[0] - 1, node[1]);
 		}
 	}
@@ -131,21 +126,21 @@ export function updateCapturedArea(arenaTiles, playerId, bounds, unfillableLocat
 		const node = queue.dequeue();
 		if (!node) continue;
 
-		const offset = node[0] * maskHeight;
+		const offset = node[0] * lineWidth;
 		if (testFillNode(node[0], node[1] + 1, offset + node[1] + 1)) {
-			byteArray[offset + node[1] + 1] = 1;
+			grid[offset + node[1] + 1] = FILLED_BLOCK;
 			queue.enqueue(node[0], node[1] + 1);
 		}
 		if (testFillNode(node[0], node[1] - 1, offset + node[1] - 1)) {
-			byteArray[offset + node[1] - 1] = 1;
+			grid[offset + node[1] - 1] = FILLED_BLOCK;
 			queue.enqueue(node[0], node[1] - 1);
 		}
-		if (testFillNode(node[0] + 1, node[1], offset + maskHeight + node[1])) {
-			byteArray[offset + maskHeight + node[1]] = 1;
+		if (testFillNode(node[0] + 1, node[1], offset + lineWidth + node[1])) {
+			grid[offset + lineWidth + node[1]] = FILLED_BLOCK;
 			queue.enqueue(node[0] + 1, node[1]);
 		}
-		if (testFillNode(node[0] - 1, node[1], offset - maskHeight + node[1])) {
-			byteArray[offset - maskHeight + node[1]] = 1;
+		if (testFillNode(node[0] - 1, node[1], offset - lineWidth + node[1])) {
+			grid[offset - lineWidth + node[1]] = FILLED_BLOCK;
 			queue.enqueue(node[0] - 1, node[1]);
 		}
 	}
@@ -158,7 +153,7 @@ export function updateCapturedArea(arenaTiles, playerId, bounds, unfillableLocat
 	let totalFilledTileCount = 0;
 	for (let x = bounds.min.x; x < bounds.max.x; x++) {
 		for (let y = bounds.min.y; y < bounds.max.y; y++) {
-			if (byteArray[x * maskHeight + y] == 0 || arenaTiles[x][y] == playerId) {
+			if (grid[x * lineWidth + y] === FILLABLE_BLOCK || grid[x * lineWidth + y] === PLAYER_BLOCK) {
 				totalFilledTileCount++;
 				newBounds.min.x = Math.min(newBounds.min.x, x);
 				newBounds.min.y = Math.min(newBounds.min.y, y);
@@ -168,14 +163,12 @@ export function updateCapturedArea(arenaTiles, playerId, bounds, unfillableLocat
 		}
 	}
 
-	// At this point, the floodFillMask contains `0` values for each tile that needs to be filled or has already been filled.
-	// We can filter out the ones that are already filled and only send rectangles of the tiles that need to be changed.
+	// pack the blocks that remains unfilled into rectangles
 	const fillRects = compressTiles(bounds, (x, y) => {
-		return byteArray[x * maskHeight + y] == 0 && arenaTiles[x][y] != playerId;
+		return grid[x * lineWidth + y] == FILLABLE_BLOCK;
 	});
 
 	return {
-		/** The rectangles that need to be filled with tiles from the player in order to fill all gaps in their area. */
 		fillRects,
 		totalFilledTileCount,
 		newBounds,
