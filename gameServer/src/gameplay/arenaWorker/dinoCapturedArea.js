cafb74e7bcdfbf97725691c927b0c0eb8d62c0aa
Dinoana <dinoana@proton.me>
2025-01-08 16:11:47 +0100

    feat(optimize): mark trail points on the mask

--- gameServer/src/gameplay/arenaWorker/dinoCapturedArea.js
+++ gameServer/src/gameplay/arenaWorker/dinoCapturedArea.js
@@ -21,6 +21,7 @@ export function dinoInitializeMask(width, height) {
 
 const EMPTY_BLOCK = 0;
 const PLAYER_BLOCK = 1;
+const PLAYER_TRAIL = 9;
 
 /**
  * @param {number[][]} arenaTiles
@@ -36,8 +37,6 @@ export function dinoCapturedArea(arenaTiles, playerId, bounds, vertices, unfilla
 	bounds.min.subScalar(1);
 	bounds.max.addScalar(1);
 
-	console.log("vertices", vertices)
-
 	// generate mask
 	for (let i = bounds.min.x; i < bounds.max.x; i++) {
 		const offset = i * lineWidth;
@@ -50,9 +49,45 @@ export function dinoCapturedArea(arenaTiles, playerId, bounds, vertices, unfilla
 		}
 	}
 
+	// fill the trail vertices on the mask
+	fillPlayerTrail(vertices, PLAYER_TRAIL);
+
 	// print mask
 	printer();
 }
+
+
+/** 
+ * @param {[x: number, y: number][]} vertices
+ * @param {number} value
+ */
+function fillPlayerTrail(vertices, value) {
+		if (vertices.length === 1) {
+			const vertex = vertices[0];
+			$matrix(vertex[0] ,vertex[1],value);
+		}
+		for (let i = 0; i < vertices.length - 1; i++) {
+			const vertexA = vertices[i];
+			const vertexB = vertices[i + 1];
+			if (vertexA[0] != vertexB[0] && vertexA[1] != vertexB[1]) {
+				throw new Error("Assertion failed, tried to fill a player trail with a diagonal edge.");
+			}
+
+			// Sort the two corners so that `min` is always in the top left.
+			const minX = Math.min(vertexA[0], vertexB[0]);
+			const minY = Math.min(vertexA[1], vertexB[1]);
+			const maxX = Math.max(vertexA[0], vertexB[0]) + 1;
+			const maxY = Math.max(vertexA[1], vertexB[1]) + 1;
+
+			for(let x = minX; x < maxX; x++){
+				for(let y = minY; y < maxY; y++){
+					$matrix(x,y,value);
+				}
+			}
+		}
+}
+
+
 
 function printer() {
 	for (let j = $bounds.min.y; j < $bounds.max.y; j++) {
@@ -62,6 +97,8 @@ function printer() {
 				line += "  ";
 			} else if ($matrix(i, j) == PLAYER_BLOCK) {
 				line += "██";
+			} else if ($matrix(i,j) == PLAYER_TRAIL) {
+				line += "▒▒"
 			}
 		}
 		console.log(line);
