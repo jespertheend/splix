ada1d412570bdedd610003ae060142eca3f9dc48
The-Protector-MSGA <214677380+The-Protector-MSGA@users.noreply.github.com>
2025-06-08 20:36:43 +0200

    Add arena gamemode

--- scripts/dev.js
+++ scripts/dev.js
@@ -42,12 +42,13 @@ generateTypes({
 	logLevel: "WARNING",
 });
 
+// pit is specific to arena gamemode and refers to the thing in the middle of the arena.
 if (!Deno.args.includes("--no-init")) {
 	const gameServer = initGameServer({
 		arenaWidth: 40,
 		arenaHeight: 40,
-		fakeArenaWidth: 16,
-		fakeArenaHeight: 16,
+		pitWidth: 16,
+		pitHeight: 16,
 		gameMode: "default",
 	});
 	// @ts-ignore
